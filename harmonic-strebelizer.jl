ad8f988b35490d88bc58bbb929e0424966c336d4
Aaron Fenyes <afenyes@math.toronto.edu>
2020-09-16 16:17:48 +0200

    Write and test Mesh constructor
    Start updating `cot_to_next` and `distortion`.

--- harmonic-strebelizer.jl
+++ harmonic-strebelizer.jl
@@ -18,32 +18,41 @@ end
 
 function Mesh(pos_next::Vector{Int}, neg_next::Vector{Int})
   # initialize the mesh's next-edge table
+  n_edges = length(pos_next)
   mesh = Mesh(pos_next, neg_next, zeros(Int, n_edges), zeros(Int, n_edges))
   
-  # `newfound[e]` is true if edge `e` points to a vertex we haven't seen yet
-  n_edges = length(pos_next)
-  newfound = trues(n_edges)
-  
+  # label the vertices
   v = 1 # the next vertex label to assign
-  for edge in 1:n_edges
+  for edge in [1:n_edges; -n_edges:-1]
     neighbor = edge
-    do
-      neighbor = mesh.next(neighbor)
-    while neighbor != edge
-    if newfound[e]
-      pos_vertex[e] = v
-      pos_vertex[pos_next[e]] = v
-      pos_vertex[pos_next[pos_next[e]]
-      newfound[e] = false
+    while (vertex(mesh, neighbor) == 0)
+      if neighbor > 0
+        mesh.pos_vertex[neighbor] = v
+      else
+        mesh.neg_vertex[-neighbor] = v
+      end
+      neighbor = next(mesh, neighbor)
+      if neighbor == edge
+        #  we're back where we started, and we're about to leave the loop
+        v += 1
+      end
+    end
+    if neighbor != edge
+      error("Edge permutation is inconsistent")
     end
   end
+  
+  # return the finished mesh
+  mesh
 end
 
 Base.length(m::Mesh) = length(m.pos_edges)
 
-# swing `e` clockwise around the vertex it points toward, and return the next
-# edge you hit
+# the edge located clockwise of `e` around the vertex `e` points toward
 next(m::Mesh, e::Int) = e > 0 ? m.pos_next[e] : m.neg_next[-e]
+
+# the vertex `e` points toward
+vertex(m::Mesh, e::Int) = e > 0 ? m.pos_vertex[e] : m.neg_vertex[-e]
 
 struct VertexFn{R}
   mesh::Mesh
@@ -81,8 +90,9 @@ end
   ##
 ##end
 
-function cot_to_next(flat_struct::DirEdgeFn, e::Int)
-  ratio = -flat_struct(e) / edge(flat_struct, next(m, e))
+function cot_to_next(flat_struct::DirEdgeFn, e)
+  neighbor = next(flat_struct.mesh, e)
+  ratio = -flat_struct(e) / flat_struct(neighbor)
   real(ratio) / imag(ratio)
 end
 
@@ -90,14 +100,46 @@ end
 function distortion(flat_struct::DirEdgeFn)
   m = flat_struct.mesh
   pos_distorts = [
-    (cot_to_next(flat_struct, next(m, e)) + cot_to_next(m, next(m, -e)))/2
+    (
+      cot_to_next(flat_struct, next(m, e))
+      + cot_to_next(flat_struct, next(m, -e))
+    ) / 2
     for e in 1:length(m)
   ]
   Mesh(m.vertices, pos_distorts, m.pos_next, m.neg_next, m.pos_vertex, m.neg_vertex, 1)
 end
 
-# --- examples ---
+# --- tests ---
 
+function test_mesh()
+  tri_mesh = Mesh([-2, -3, -1], [3, 1, 2])
+  tri_tests = [
+    [
+      length(unique(tri_mesh.pos_vertex)) == 3,
+      length(unique(tri_mesh.neg_vertex)) == 3
+    ];
+    [vertex(tri_mesh, e) == vertex(tri_mesh, -mod1(e+1, 3)) for e in 1:3]
+  ]
+  
+  key_mesh = Mesh([-2, -3, -1], [3, 2, 1])
+  key_tests = [
+    [
+      length(unique(key_mesh.pos_vertex)) == 2,
+      length(unique(key_mesh.neg_vertex)) == 2
+    ];
+    [
+      vertex(key_mesh, -1) == vertex(key_mesh, 3),
+      vertex(key_mesh, 3) == vertex(key_mesh, -1),
+      vertex(key_mesh, 1) == vertex(key_mesh, -2),
+      vertex(key_mesh, -2) == vertex(key_mesh, 2),
+      vertex(key_mesh, 2) == vertex(key_mesh, -3)
+    ]
+  ]
+  
+  [tri_tests, key_tests]
+end
+
+# --- examples ---
 
 function torus_mesh(a_edge::R, b_edge::R, a_res, b_res) where R
   N = a_res * b_res

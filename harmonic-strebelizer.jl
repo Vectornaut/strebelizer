610858af8b66ada372cc3b19a25a310790f615f4
Aaron Fenyes <afenyes@math.toronto.edu>
2020-09-16 18:31:18 +0200

    Finish updating energy density and examples

--- harmonic-strebelizer.jl
+++ harmonic-strebelizer.jl
@@ -2,7 +2,7 @@ module Strebelizer
 
 using LinearAlgebra, Plots
 
-export Mesh, edge, next, show, cot_to_next, distortion, example
+export Mesh, edge, next, cot_to_next, distortion, example
 
 ## to do:
 ## - find distance functions to the two sides of a non-separating curve
@@ -46,7 +46,7 @@ function Mesh(pos_next::Vector{Int}, neg_next::Vector{Int})
   mesh
 end
 
-Base.length(m::Mesh) = length(m.pos_edges)
+Base.length(m::Mesh) = length(m.pos_next)
 
 # the edge located clockwise of `e` around the vertex `e` points toward
 next(m::Mesh, e::Int) = e > 0 ? m.pos_next[e] : m.neg_next[-e]
@@ -76,11 +76,11 @@ end
 
 (w::DirEdgeFn)(e) = e > 0 ? w.pos_values[e] : w.neg_values[-e]
 
-function show(m::Mesh)
-  println("reverser: ", m.reverser)
+function Base.show(m::Mesh)
   for e in 1:length(m)
-    println(e, ": ", m.pos_edges[e])
+    println(e, ": ", vertex(m, -e), " --> ", vertex(m, e))
   end
+  println()
   for e in 1:length(m)
     println(e, " --> ", next(m, e), "\t\t", -e, " --> ", next(m, -e))
   end
@@ -96,17 +96,20 @@ function cot_to_next(flat_struct::DirEdgeFn, e)
   real(ratio) / imag(ratio)
 end
 
-# assume m is Delaunay (no checks for now)
-function distortion(flat_struct::DirEdgeFn)
+function cot_to_next(flat_struct::DirEdgeFn)
   m = flat_struct.mesh
-  pos_distorts = [
-    (
-      cot_to_next(flat_struct, next(m, e))
-      + cot_to_next(flat_struct, next(m, -e))
-    ) / 2
-    for e in 1:length(m)
-  ]
-  Mesh(m.vertices, pos_distorts, m.pos_next, m.neg_next, m.pos_vertex, m.neg_vertex, 1)
+  DirEdgeFn(
+    m,
+    [cot_to_next(flat_struct, e) for e in 1:length(m)],
+    [cot_to_next(flat_struct, -e) for e in 1:length(m)]
+  )
+end
+
+# assume m is Delaunay (no checks for now)
+function energy_density(flat_struct::DirEdgeFn)
+  m = flat_struct.mesh
+  r = cot_to_next(flat_struct)
+  EdgeFn(m, [abs2(flat_struct(e)) * (r(e) + r(-e)) / 2 for e in 1:length(m)])
 end
 
 # --- tests ---
@@ -141,46 +144,43 @@ end
 
 # --- examples ---
 
-function torus_mesh(a_edge::R, b_edge::R, a_res, b_res) where R
+function flat_torus(a_charge, b_charge, a_res, b_res)
   N = a_res * b_res
-  pos_edges = repeat([a_edge, b_edge, -a_edge - b_edge], N)
+  pos_charge = repeat([a_charge, b_charge, -(a_charge + b_charge)], N)
   
   pos_next = zeros(Int, 3*a_res*b_res)
   neg_next = zeros(Int, 3*a_res*b_res)
   for i in 0:(a_res-1), j in 0:(b_res-1)
     k = 3*(i + a_res*j)
-    pos_next[k + 1] = k + 2
-    pos_next[k + 2] = k + 3
-    pos_next[k + 3] = k + 1
+    pos_next[k + 1] = -(k + 2)
+    pos_next[k + 2] = -(k + 3)
+    pos_next[k + 3] = -(k + 1)
     
     e1 = k + 3
     e2 = 3a_res*mod(j+1, b_res) + 3i + 1
     e3 = 3a_res*j + 3mod(i-1, a_res) + 2
-    neg_next[e1] = -e2
-    neg_next[e2] = -e3
-    neg_next[e3] = -e1
+    neg_next[e1] = e2
+    neg_next[e2] = e3
+    neg_next[e3] = e1
   end
   
-  Mesh{R}(pos_edges, pos_next, neg_next, -1)
+  DirEdgeFn(Mesh(pos_next, neg_next), pos_charge, -pos_charge)
 end
 
 function dumb_example()
-  m = Mesh{Complex{Float64}}(
-    [1im, -1, 1-1im],
-    [2, 3, 1],
-    [-2, -3, -1],
-    -1
-  )
+  m = Mesh([-2, -3, -1], [3, 1, 2])
+  pos_charge = [1, im, -(1+im)]
+  flat_struct = DirEdgeFn(m, pos_charge, -pos_charge)
   show(m)
   println()
-  show(distortion(m))
+  show(energy_density(flat_struct).values)
 end
 
 function example()
-  m = torus_mesh(complex(1.0), -0.1 + 1.0im, 2, 3)
-  show(m)
+  flat_struct = flat_torus(1, -0.1 + im, 3, 2)
+  show(flat_struct.mesh)
   println()
-  show(distortion(m))
+  show(energy_density(flat_struct).values)
 end
 
 end

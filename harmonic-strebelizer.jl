279b272a20d9a9eb665a837a955c66a95d7fd87d
Aaron Fenyes <afenyes@math.toronto.edu>
2020-09-16 20:25:00 +0200

    Observe that a Mesh represents a ribbon graph

--- harmonic-strebelizer.jl
+++ harmonic-strebelizer.jl
@@ -9,29 +9,29 @@ export Mesh, edge, next, cot_to_next, distortion, example
 ## - calculate gradient flow carefully
 ## - figure out how to treat separating case?
 
-struct Mesh
+struct RibbonGraph
   pos_next::Vector{Int}
   neg_next::Vector{Int}
   pos_vertex::Vector{Int}
   neg_vertex::Vector{Int}
 end
 
-function Mesh(pos_next::Vector{Int}, neg_next::Vector{Int})
+function RibbonGraph(pos_next::Vector{Int}, neg_next::Vector{Int})
   # initialize the mesh's next-edge table
   n_edges = length(pos_next)
-  mesh = Mesh(pos_next, neg_next, zeros(Int, n_edges), zeros(Int, n_edges))
+  graph = RibbonGraph(pos_next, neg_next, zeros(Int, n_edges), zeros(Int, n_edges))
   
   # label the vertices
   v = 1 # the next vertex label to assign
   for edge in [1:n_edges; -n_edges:-1]
     neighbor = edge
-    while (vertex(mesh, neighbor) == 0)
+    while (vertex(graph, neighbor) == 0)
       if neighbor > 0
-        mesh.pos_vertex[neighbor] = v
+        graph.pos_vertex[neighbor] = v
       else
-        mesh.neg_vertex[-neighbor] = v
+        graph.neg_vertex[-neighbor] = v
       end
-      neighbor = next(mesh, neighbor)
+      neighbor = next(graph, neighbor)
       if neighbor == edge
         #  we're back where we started, and we're about to leave the loop
         v += 1
@@ -43,51 +43,51 @@ function Mesh(pos_next::Vector{Int}, neg_next::Vector{Int})
   end
   
   # return the finished mesh
-  mesh
+  graph
 end
 
-Base.length(m::Mesh) = length(m.pos_next)
+Base.length(g::RibbonGraph) = length(g.pos_next)
 
 # the edge located clockwise of `e` around the vertex `e` points toward
-next(m::Mesh, e::Int) = e > 0 ? m.pos_next[e] : m.neg_next[-e]
+next(g::RibbonGraph, e::Int) = e > 0 ? g.pos_next[e] : g.neg_next[-e]
 
 # the vertex `e` points toward
-vertex(m::Mesh, e::Int) = e > 0 ? m.pos_vertex[e] : m.neg_vertex[-e]
+vertex(g::RibbonGraph, e::Int) = e > 0 ? g.pos_vertex[e] : g.neg_vertex[-e]
 
 struct VertexFn{R}
-  mesh::Mesh
+  graph::RibbonGraph
   values::Vector{R}
 end
 
 (f::VertexFn)(v) = f.values[v]
 
 struct EdgeFn{R}
-  mesh::Mesh
+  graph::RibbonGraph
   values::Vector{R}
 end
 
 (w::EdgeFn)(e) = w.values[e]
 
 struct DirEdgeFn{R}
-  mesh::Mesh
+  graph::RibbonGraph
   pos_values::Vector{R}
   neg_values::Vector{R}
 end
 
 (w::DirEdgeFn)(e) = e > 0 ? w.pos_values[e] : w.neg_values[-e]
 
-function Base.show(m::Mesh)
-  e_form = string("{: 0", 1 + length(digits(length(m))), "d}")
-  v_form = string("{:0", length(digits(maximum([m.pos_vertex; m.neg_vertex]))), "d}")
+function Base.show(g::RibbonGraph)
+  e_form = string("{: 0", 1 + length(digits(length(g))), "d}")
+  v_form = string("{:0", length(digits(maximum([g.pos_vertex; g.neg_vertex]))), "d}")
   to_endpts = FormatExpr(string(e_form, ": ", v_form, " --> ", v_form))
   next_form = string(e_form, " --> ", e_form)
   to_next = FormatExpr(string(next_form, "   ", next_form))
-  for e in 1:length(m)
-    printfmtln(to_endpts, e, vertex(m, -e), vertex(m, e))
+  for e in 1:length(g)
+    printfmtln(to_endpts, e, vertex(g, -e), vertex(g, e))
   end
   println()
-  for e in 1:length(m)
-    printfmtln(to_next, e, next(m, e), -e, next(m, -e))
+  for e in 1:length(g)
+    printfmtln(to_next, e, next(g, e), -e, next(g, -e))
   end
 end
 
@@ -96,55 +96,55 @@ end
 ##end
 
 function cot_to_next(flat_struct::DirEdgeFn, e)
-  neighbor = next(flat_struct.mesh, e)
+  neighbor = next(flat_struct.graph, e)
   ratio = -flat_struct(e) / flat_struct(neighbor)
   real(ratio) / imag(ratio)
 end
 
 function cot_to_next(flat_struct::DirEdgeFn)
-  m = flat_struct.mesh
+  graph = flat_struct.graph
   DirEdgeFn(
-    m,
-    [cot_to_next(flat_struct, e) for e in 1:length(m)],
-    [cot_to_next(flat_struct, -e) for e in 1:length(m)]
+    graph,
+    [cot_to_next(flat_struct, e) for e in 1:length(graph)],
+    [cot_to_next(flat_struct, -e) for e in 1:length(graph)]
   )
 end
 
 # assume m is Delaunay (no checks for now)
 function energy_density(flat_struct::DirEdgeFn)
-  m = flat_struct.mesh
+  g = flat_struct.graph
   r = cot_to_next(flat_struct)
-  EdgeFn(m, [abs2(flat_struct(e)) * (r(e) + r(-e)) / 2 for e in 1:length(m)])
+  EdgeFn(g, [abs2(flat_struct(e)) * (r(e) + r(-e)) / 2 for e in 1:length(g)])
 end
 
 # --- tests ---
 
-function test_mesh()
-  tri_mesh = Mesh([-2, -3, -1], [3, 1, 2])
-  tri_tests = [
+function test_graphs()
+  triangle = RibbonGraph([-2, -3, -1], [3, 1, 2])
+  triangle_tests = [
     [
-      length(unique(tri_mesh.pos_vertex)) == 3,
-      length(unique(tri_mesh.neg_vertex)) == 3
+      length(unique(triangle.pos_vertex)) == 3,
+      length(unique(triangle.neg_vertex)) == 3
     ];
-    [vertex(tri_mesh, e) == vertex(tri_mesh, -mod1(e+1, 3)) for e in 1:3]
+    [vertex(triangle, e) == vertex(triangle, -mod1(e+1, 3)) for e in 1:3]
   ]
   
-  key_mesh = Mesh([-2, -3, -1], [3, 2, 1])
-  key_tests = [
+  heavy8 = RibbonGraph([-2, -3, -1], [3, 2, 1])
+  heavy8_tests = [
     [
-      length(unique(key_mesh.pos_vertex)) == 2,
-      length(unique(key_mesh.neg_vertex)) == 2
+      length(unique(heavy8.pos_vertex)) == 2,
+      length(unique(heavy8.neg_vertex)) == 2
     ];
     [
-      vertex(key_mesh, -1) == vertex(key_mesh, 3),
-      vertex(key_mesh, 3) == vertex(key_mesh, -1),
-      vertex(key_mesh, 1) == vertex(key_mesh, -2),
-      vertex(key_mesh, -2) == vertex(key_mesh, 2),
-      vertex(key_mesh, 2) == vertex(key_mesh, -3)
+      vertex(heavy8, -1) == vertex(heavy8, 3),
+      vertex(heavy8, 3) == vertex(heavy8, -1),
+      vertex(heavy8, 1) == vertex(heavy8, -2),
+      vertex(heavy8, -2) == vertex(heavy8, 2),
+      vertex(heavy8, 2) == vertex(heavy8, -3)
     ]
   ]
   
-  [tri_tests, key_tests]
+  [triangle_tests, heavy8_tests]
 end
 
 # --- examples ---
@@ -169,21 +169,21 @@ function flat_torus(a_charge, b_charge, a_res, b_res)
     neg_next[e3] = e1
   end
   
-  DirEdgeFn(Mesh(pos_next, neg_next), pos_charge, -pos_charge)
+  DirEdgeFn(RibbonGraph(pos_next, neg_next), pos_charge, -pos_charge)
 end
 
 function dumb_example()
-  m = Mesh([-2, -3, -1], [3, 1, 2])
+  g = RibbonGraph([-2, -3, -1], [3, 1, 2])
   pos_charge = [1, im, -(1+im)]
-  flat_struct = DirEdgeFn(m, pos_charge, -pos_charge)
-  show(m)
+  flat_struct = DirEdgeFn(g, pos_charge, -pos_charge)
+  show(g)
   println()
   show(energy_density(flat_struct).values)
 end
 
 function example()
   flat_struct = flat_torus(1, -0.1 + im, 3, 2)
-  show(flat_struct.mesh)
+  show(flat_struct.graph)
   println()
   show(energy_density(flat_struct).values)
 end

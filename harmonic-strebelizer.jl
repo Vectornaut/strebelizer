19272c0cbffa7a34e260374ec5bb2373174d9978
Aaron Fenyes <afenyes@math.toronto.edu>
2020-09-16 14:12:59 +0200

    Represent functions on vertices, edges, and directed edges
    Separate the representation of functions from the representation of
    meshes. The code is half-baked right now: it won't run, and it wouldn't
    work correctly if it did run.

--- harmonic-strebelizer.jl
+++ harmonic-strebelizer.jl
@@ -4,31 +4,68 @@ using LinearAlgebra, Plots
 
 export Mesh, edge, next, show, cot_to_next, distortion, example
 
-struct Mesh{S, R}
-  vertices::Vector{S}
-  pos_edges::Vector{R}
+## to do:
+## - find distance functions to the two sides of a non-separating curve
+## - calculate gradient flow carefully
+## - figure out how to treat separating case?
+
+struct Mesh
   pos_next::Vector{Int}
   neg_next::Vector{Int}
-  reverser
+  pos_vertex::Vector{Int}
+  neg_vertex::Vector{Int}
+end
+
+function Mesh(pos_next::Vector{Int}, neg_next::Vector{Int})
+  # initialize the mesh's next-edge table
+  mesh = Mesh(pos_next, neg_next, zeros(Int, n_edges), zeros(Int, n_edges))
+  
+  # `newfound[e]` is true if edge `e` points to a vertex we haven't seen yet
+  n_edges = length(pos_next)
+  newfound = trues(n_edges)
+  
+  v = 1 # the next vertex label to assign
+  for edge in 1:n_edges
+    neighbor = edge
+    do
+      neighbor = mesh.next(neighbor)
+    while neighbor != edge
+    if newfound[e]
+      pos_vertex[e] = v
+      pos_vertex[pos_next[e]] = v
+      pos_vertex[pos_next[pos_next[e]]
+      newfound[e] = false
+    end
+  end
 end
 
 Base.length(m::Mesh) = length(m.pos_edges)
 
-function edge(m::Mesh, e::Int)
-  if e > 0
-    m.pos_edges[e]
-  else
-    m.reverser * m.pos_edges[-e]
-  end
+# swing `e` clockwise around the vertex it points toward, and return the next
+# edge you hit
+next(m::Mesh, e::Int) = e > 0 ? m.pos_next[e] : m.neg_next[-e]
+
+struct VertexFn{R}
+  mesh::Mesh
+  values::Vector{R}
 end
 
-function next(m::Mesh, e::Int)
-  if e > 0
-    m.pos_next[e]
-  else
-    m.neg_next[-e]
-  end
+(f::VertexFn)(v) = f.values[v]
+
+struct EdgeFn{R}
+  mesh::Mesh
+  values::Vector{R}
 end
+
+(w::EdgeFn)(e) = w.values[e]
+
+struct DirEdgeFn{R}
+  mesh::Mesh
+  pos_values::Vector{R}
+  neg_values::Vector{R}
+end
+
+(w::DirEdgeFn)(e) = e > 0 ? w.pos_values[e] : w.neg_values[-e]
 
 function show(m::Mesh)
   println("reverser: ", m.reverser)
@@ -44,18 +81,19 @@ end
   ##
 ##end
 
-function cot_to_next(m::Mesh, e::Int)
-  ratio = -edge(m, e) / edge(m, next(m, e))
+function cot_to_next(flat_struct::DirEdgeFn, e::Int)
+  ratio = -flat_struct(e) / edge(flat_struct, next(m, e))
   real(ratio) / imag(ratio)
 end
 
 # assume m is Delaunay (no checks for now)
-function distortion(m::Mesh)
+function distortion(flat_struct::DirEdgeFn)
+  m = flat_struct.mesh
   pos_distorts = [
-    (cot_to_next(m, next(m, e)) + cot_to_next(m, next(m, -e)))/2
+    (cot_to_next(flat_struct, next(m, e)) + cot_to_next(m, next(m, -e)))/2
     for e in 1:length(m)
   ]
-  Mesh(pos_distorts, m.pos_next, m.neg_next, 1)
+  Mesh(m.vertices, pos_distorts, m.pos_next, m.neg_next, m.pos_vertex, m.neg_vertex, 1)
 end
 
 # --- examples ---
